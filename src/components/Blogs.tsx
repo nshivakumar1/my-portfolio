d36458d6b0321df8c8b79027f0057a240073878c
Nakul Shivakumar <codecloudevops@outlook.com>
2025-09-07 15:20:24 +0530

    Updated modules

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { BlogPost } from '../types';
+import styles from '../styles/Blogs.module.css';
 
 interface BlogsProps {
   posts: BlogPost[];
@@ -7,20 +8,24 @@ interface BlogsProps {
 
 const Blogs: React.FC<BlogsProps> = ({ posts }) => {
   return (
-    <div>
-      <h1>Blog Posts</h1>
-      <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <a href={post.link} target="_blank" rel="noopener noreferrer">
-              {post.title}
-            </a>{' '}
-            {post.pubDate && (
-              <span>- {new Date(post.pubDate).toLocaleDateString()}</span>
-            )}
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Blog Posts</h1>
+      {posts.length === 0 ? (
+        <div className={styles.empty}>No posts available right now.</div>
+      ) : (
+        <ul className={styles.list}>
+          {posts.map((post, index) => (
+            <li key={index} className={styles.item}>
+              <a className={styles.link} href={post.link} target="_blank" rel="noopener noreferrer">
+                {post.title}
+              </a>
+              {post.pubDate && (
+                <span className={styles.date}>{new Date(post.pubDate).toLocaleDateString()}</span>
+              )}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
